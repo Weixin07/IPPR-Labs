30a4736fe20c5a442cbe5bc2fd913bc2c6b6e5a5
Faithlin Hoe <faithlin07@gmail.com>
2023-11-24 15:32:32 +0800

    Update ippr24112023.m

--- ippr24112023.m
+++ ippr24112023.m
@@ -1,4 +1,5 @@
-% Colour Image Processing
+%% Colour Image Processing
+%{
 im = imread("standard_test_images\lena_color_512.tif");
 [m, n, o]=size(im);
 R = cat(3, im(:,:,1), zeros(n), zeros(n));
@@ -9,7 +10,7 @@ subplot(3,4,2), imshow(R), title('Red Channel');
 subplot(3,4,3), imshow(G), title('Green Channel');
 subplot(3,4,4), imshow(B), title('Blue Channel');
 
-% 2. %RGB --> CMY
+% RGB --> CMY
 f1 = cat(3, im(:,:,1), im(:,:,2)*0+255, im(:,:,3)*0+255);
 f2 = cat(3, im(:,:,1)*0+255, im(:,:,2) , im(:,:,3)*0+255);
 f3 = cat(3, im(:,:,1)*0+255, im(:,:,2)*0+255, im(:,:,3));
@@ -18,11 +19,11 @@ subplot(3,4,6), imshow(f1), title('Cyan Channel');
 subplot(3,4,7), imshow(f2), title('Magenta Channel');
 subplot(3,4,8), imshow(f3), title('Yellow Channel');
 
-% 3. %Color Image Enhancement - Contrast
+% Color Image Enhancement - Contrast
 En = cat(3, im(:,:,1)./2,im(:,:,2)./2,im(:,:,3)./2);
 subplot(3,4,9), imshow(En), title('Colour Image Enhancement');
 
-% 4. %Colour Image Edge Detection
+% Colour Image Edge Detection
 img1=rgb2ycbcr(im);
 dx1=edge(img1(:,:,1),'canny');
 dx1=(dx1*255);
@@ -42,3 +43,36 @@ Sh = ((H>0.49)+(H<0.08))>0;
 Sh = double(bwmorph(Sh,'dilate',3));
 Sh = double(bwmorph(Sh,'erode',3));
 subplot(3,4,11), imshow(Sh), title('Colour Image Segmentation');
+%}
+
+%% Image Inversion
+ima = imread("standard_test_images\Peppers.png");
+ima_out = invert(ima);
+
+function ima_out = invert(ima)
+% Checks that ima is a gray level image
+if ndims(ima) > 2
+    % RGB image?
+    if ndims(ima) == 3
+        ima = double(rgb2gray(uint8(ima)));
+    else
+        disp('Unknown format, Cannot guarantee result');
+    end
+end
+
+figure;
+imagesc(ima);
+ima = double(ima);
+colormap(gray);
+axis(image);
+mini = min(min(ima));
+maxi = max(max(ima));
+ima_out = (ima-mini)/(maxi-mini)*255;
+ima_out = 255-ima_out;
+
+figure;
+imagesc(ima_out);
+colormap(gray);
+axis image;
+axis off;
+end
